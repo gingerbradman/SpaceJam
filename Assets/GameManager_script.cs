dc2b30f38a06a461f0731c8986f89987659bbbe5
gingerbradman <lordbreadly8@gmail.com>
2021-06-10 15:34:24 -0500

    Initial Commit

---
+++ Assets/GameManager_script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_script : MonoBehaviour
+{
+
+    public GameObject obstacleLine;
+
+    private IEnumerator coroutine;
+
+    void Start()
+    {
+        coroutine = CreateObstacle(2.0f);
+        StartCoroutine(coroutine);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator CreateObstacle(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            GameObject clone;
+            clone = Instantiate(obstacleLine, this.transform.position, this.transform.rotation);
+            clone.GetComponent<Rigidbody2D>().velocity = Vector2.down * 3;
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D r;
+    public void setR(Rigidbody2D rigidBody){ r = rigidBody;}
+    private float horizontalMovement;
+    private float verticalMovement;
+    bool isWrappingX = false;
+    bool isWrappingY = false;
+
+    Renderer[] renderers;
+    Camera cam;
+    Vector2 viewportPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        setR(this.GetComponent<Rigidbody2D>());
+        renderers = GetComponentsInChildren<Renderer>();
+        cam = Camera.main;
+        viewportPosition = cam.WorldToViewportPoint(transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        ScreenWrap();
+    }
+
+    void Movement()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
+        r.position += new Vector2( horizontalMovement, verticalMovement) * speed * Time.deltaTime;
+    }
+
+    bool CheckRenderers()
+    {
+        foreach(var renderer in renderers)
+        {
+            // If at least one render is visible, return true
+            if(renderer.isVisible)
+            {
+                return true;
+            }
+        }
+    
+        // Otherwise, the object is invisible
+        return false;
+    }
+    
+    void ScreenWrap()
+    {
+        var isVisible = CheckRenderers();
+    
+        if(isVisible)
+        {
+            isWrappingX = false;
+            isWrappingY = false;
+            return;
+        }
+    
+        if(isWrappingX && isWrappingY) {
+            return;
+        }
+    
+        var cam = Camera.main;
+        var viewportPosition = cam.WorldToViewportPoint(transform.position);
+        var newPosition = transform.position;
+    
+        if (!isWrappingX && (viewportPosition.x > 1 || viewportPosition.x < 0))
+        {
+            newPosition.x = -newPosition.x;
+    
+            isWrappingX = true;
+        }
+    
+        if (!isWrappingY && (viewportPosition.y > 1 || viewportPosition.y < 0))
+        {
+            newPosition.y = -newPosition.y;
+    
+            isWrappingY = true;
+        }
+    
+        transform.position = newPosition;
+    }
+}
